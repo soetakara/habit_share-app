c1b16d3711957e2845558408f89bd0b5a8fd4bff
soetakara <soetakara.717@gmail.com>
2021-12-11 11:38:33 +0900

    インスタンス変数の設定

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,12 @@
 class RoomsController < ApplicationController
+  before_action :room_set, only: [:new]
   def index
   end
   def new
+  end
+
+  private
+  def room_set
+    @room = Room.new
   end
 end
