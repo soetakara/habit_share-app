ca59611da5281134414ae75c9c4a92bde226ef3e
soetakara <soetakara.717@gmail.com>
2021-12-12 17:09:24 +0900

    リストの新規登録機能実装

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,4 +1,17 @@
 class HabitsController < ApplicationController
   def new
+    @habit = Habit.new
+  end
+  def create
+    @habit = Habit.new(habit_params)
+    if @habit.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+  private
+  def habit_params
+    params.require(:habit).permit(:title, :item1, :item2, :item3, :item4, :item5).merge(user_id: current_user.id)
   end
 end
