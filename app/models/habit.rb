c2c73eea7e9cca00eb6449c9e2436851f3d0a042
soetakara <soetakara.717@gmail.com>
2021-12-17 03:00:40 +0900

    アソシエーションへのdestroy反映

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -3,7 +3,7 @@ class Habit < ApplicationRecord
   validates :item1, presence: true 
 
   belongs_to :user
-  has_many :room_habits
+  has_many :room_habits, dependent: :destroy
   has_many :rooms, through: :room_habits
 
 end
