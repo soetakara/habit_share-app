51ab4dfcfe42fe6ed7cdef4694fbbd992f14accc
soetakara <soetakara.717@gmail.com>
2021-12-17 01:09:28 +0900

    habitデータ未登録時の条件分岐

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,7 @@ class MessagesController < ApplicationController
     @messages = @room.messages.includes(:user)
     @habits = @room.habits
     @my_habits = Habit.where(user_id: current_user.id)
+    @not_habits = not_set_habit
   end 
   def create
     @room = Room.find(params[:room_id])
@@ -19,5 +20,21 @@ class MessagesController < ApplicationController
   private
   def message_params
     params.require(:message).permit(:comment).merge(user_id: current_user.id)
+  end
+  def not_set_habit
+    if @room.user_ids.length > @habits.length
+      habits = []
+        @habits.each do |habit|
+          habits << habit.user_id
+        end
+        not_habit = []
+        @room.user_ids.each do |user|
+           if habits.include?(user)
+           elsif 
+            not_habit << User.find(user)
+           end
+        end
+        return(not_habit)
+    end
   end
 end
